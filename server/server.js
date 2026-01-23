b7763f477d21acf686fa3d212756908053625209
Charles <adeakinyemi129@gmail.com>
2026-01-23 20:48:50 +0100

    added a subprocess

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const mongoose = require("mongoose")
 
 
 const app = express()
@@ -8,7 +9,11 @@ app.get("/", (req, res) => {
     res.send("Hello, World")
 })
 
-
+process.on("SIGINT", async () => {
+    console.log("Gracefully shutting down...")
+    await mongoose.connection.close()
+    process.exit(0)
+})
 
 app.listen(3000, ()=> {
     console.log("Server is running on port 3000")
