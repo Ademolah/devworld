c200387447b182bbc7e8c5a23a035ca70878db40
Charles <adeakinyemi129@gmail.com>
2026-02-11 09:55:24 +0100

    added github action, prettier lint

--- server/server.js
+++ server/server.js
@@ -1,20 +1,25 @@
-const express = require("express")
-const mongoose = require("mongoose")
+const express = require("express");
+const mongoose = require("mongoose");
 
 
-const app = express()
+const app = express();
 
 
 app.get("/", (req, res) => {
-    res.send("Hello, World")
-})
+    res.send("Hello, World");
+});
+
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to Mongodb successfully"))
+  .catch((error) => console.log("Error connecting to Mongodb:", error));
 
 process.on("SIGINT", async () => {
-    console.log("Gracefully shutting down...")
-    await mongoose.connection.close()
-    process.exit(0)
-})
+    console.log("Gracefully shutting down...");
+    await mongoose.connection.close();
+    process.exit(0);
+});
 
 app.listen(3000, ()=> {
-    console.log("Server is running on port 3000")
-})
+    console.log("Server is running on port 3000");
+});
